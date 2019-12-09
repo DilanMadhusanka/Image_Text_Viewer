ac00319842006168ccd63e950811022cd6d4f352
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2019-12-09 21:57:30 +0530

    upload image instead give the file path

--- Image-Text-Viewer/src/main/java/com/project/imageTextViewer/Image/Text/Viewer/controller/ImageController.java
+++ Image-Text-Viewer/src/main/java/com/project/imageTextViewer/Image/Text/Viewer/controller/ImageController.java
@@ -1,11 +1,14 @@
 package com.project.imageTextViewer.Image.Text.Viewer.controller;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.project.imageTextViewer.Image.Text.Viewer.jpa.ImageRepository;
 import com.project.imageTextViewer.Image.Text.Viewer.model.ImageModel;
@@ -21,12 +24,21 @@ public class ImageController {
 	ImageRepository imageRepository;
 	
 	@RequestMapping("/read")
-	public String readText(@RequestParam String filePath) {
-		File imageFile = new File(filePath);
+	public String readText(@RequestParam MultipartFile filePath) throws IOException {
+		File imageFile = convert(filePath);
 		String results = readText.readText(imageFile);
-		imageRepository.save(new ImageModel(results, filePath));
+		imageRepository.save(new ImageModel(results, ""));
 		return results;
         
+    }
+	
+	public static File convert(MultipartFile file) throws IOException {
+        File convFile = new File(file.getOriginalFilename());
+        convFile.createNewFile();
+        FileOutputStream fos = new FileOutputStream(convFile);
+        fos.write(file.getBytes());
+        fos.close();
+        return convFile;
     }
 	
 }
